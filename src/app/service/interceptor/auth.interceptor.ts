285e1c3a239a696ba8d656c4199b17e4d63cb5ef
Cristian686504 <cristian.renz@estudiantes.utec.edu.uy>
2025-04-06 23:34:40 -0300

    Arreglo de error 403 de las peticiones de admin

--- src/app/service/interceptor/auth.interceptor.ts
+++ src/app/service/interceptor/auth.interceptor.ts
@@ -1,11 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { ClienteService } from '../clienteService/cliente.service';
+import { AdministradorService } from '../administradorService/administrador.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const administradorService = inject(AdministradorService);
+  const currentAdminUser = administradorService.currentUserValue;
   const clienteService = inject(ClienteService);
   const currentUser = clienteService.currentUserValue;
-  
+
   if (currentUser && currentUser.token) {
     console.log(currentUser.token);
     req = req.clone({
@@ -13,7 +16,12 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
         Authorization: `Bearer ${currentUser.token}`
       }
     });
+  } else if (currentAdminUser && currentAdminUser.token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${currentAdminUser.token}`
+      }
+    });
   }
-  
   return next(req);
 };
