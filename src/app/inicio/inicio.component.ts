b3aea60a3bb6f3efde13d8a37c87b870404454b6
juanbmathieu <bautimathieu13@gmail.com>
2025-06-17 21:34:34 -0300

    Cambios a Inicio y alertas

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { ClienteService } from '../service/clienteService/cliente.service';
+import { AdministradorService } from '../service/administradorService/administrador.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports : [ CommonModule, MatIconModule, RouterModule ],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrls: ['./inicio.component.scss']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit, OnDestroy {
+  currentUser: any = null;
+  currentUserAdmin: any = null;
 
+  mensajes = [
+    '🚚 Delivery en toda la ciudad',
+    '🌱 Ingredientes frescos y naturales',
+    '🎉 Fiestas personalizadas para vos'
+  ];
+  
+  mensajeTira = this.mensajes[0];
+  private intervalo: any;
+  
+  
+  constructor(
+    private clienteService: ClienteService,
+    private administradorService: AdministradorService) {
+    this.iniciarTira();
+  }
+
+  ngOnInit() {
+    this.clienteService.currentUser.subscribe(user => {
+      if (user) {
+        this.currentUser = user;
+        this.currentUserAdmin = null;
+      } else {
+        this.checkAdminUser();
+      }
+    });
+  }
+  
+
+  checkAdminUser() {
+    this.administradorService.currentUser.subscribe(user => {
+      if (user) {
+        this.currentUserAdmin = user;
+        this.currentUser = null; 
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalo) {
+      clearInterval(this.intervalo);
+    }
+  }
+
+  private iniciarTira(): void {
+    let i = 0;
+    this.intervalo = setInterval(() => {
+      i = (i + 1) % this.mensajes.length;
+      this.mensajeTira = this.mensajes[i];
+    }, 4000);
+  }
 }
